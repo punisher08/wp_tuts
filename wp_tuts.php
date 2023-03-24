14606638eea5169f173629c097fdf49a3fc8e528
jdev.netseek <jdev.netseek@gmail.com>
2023-03-24 10:55:15 +0800

    initialize plugin details

---
+++ wp_tuts.php
@@ -0,0 +1,18 @@
+<?php
+/** 
+ * @package WP tutorials
+ * Plugin Name:		    WP tutorials
+ * Plugin URI:			https://jretech.com/
+ * Description:			Basic WP tutorial
+ * Version:				1.0.0
+ * Requires at least: 	6.0
+ * Requires PHP:        5.2
+ * Tested up to:		6.1
+ * Author:				jhomignacio08@gmail.com
+ * Author URI:			https://jretech.com/
+ * License:    			GPL2 or later
+ * License URI:			https://www.gnu.org/licenses/gpl-2.0.html
+ * 
+ */
+
+
